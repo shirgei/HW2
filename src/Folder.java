651136a0beace22021b1383abd32d4f499963109
Shir <shir.g@campus.technion.ac.il>
2021-06-18 17:58:53 +0300

    new temp

--- src/Folder.java
+++ src/Folder.java
@@ -1,22 +1,35 @@
 import java.util.ArrayList;
 
 public class Folder extends StorageItem {
-    private ArrayList<Folder> folder;
+    private ArrayList<StorageItem> folder;
     private File file;
 
     public Folder(String name) {
         super(name);
-        this.folder = new ArrayList<Folder>();
+        this.folder = new ArrayList<>();
         //check what to do with folder and file
     }
 
+
+    @Override
     public int getSize() {
         int totalSize = 0;
-        for (int i = 0; i < this.folder.size(); i++) {
-            if (this.folder.get(i) instanceof File) {
-                totalSize += this.folder[i].getSize();
+        for (int i = 0; i < folder.size(); i++) {
+            totalSize += folder.get(i).getSize();
+        }
+        return totalSize;
+    }
+
+    public boolean addItem(StorageItem item){
+        boolean add = true;
+        for (int i = 0; i < folder.size(); i++){
+            if(item.getName()==this.folder.get(i).getName()){
+                return false;
+            }
+            if(folder.get(i) instanceof Folder){
+                (Folder)this.folder.get(i).addItem(item);
             }
         }
-
     }
 }
+
